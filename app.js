b9e6124fb1348fe3f813d146dce83cfbcecab304
gguo <gguo@movoto.com>
2016-09-29 13:10:02 +0800

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.render("home.ejs", { "name": "George" });
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
