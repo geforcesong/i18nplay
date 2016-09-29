93790899c762be36f6ce311c7835c3e145be579c
gguo <gguo@movoto.com>
2016-09-29 15:00:11 +0800

    hello

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ module.exports = function (app) {
         else if(req.query.lan == 'es')
             req.i18n.changeLanguage('es');
         console.log( `current language is: ${req.i18n.language}` );
+        
         console.log(req.t('HI'));
         res.render("home.ejs", { "name": "George" });
     });
